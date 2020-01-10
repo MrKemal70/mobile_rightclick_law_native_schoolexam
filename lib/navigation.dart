57bc526445cb3553069ce9547cb6b42eadac1fa8
Kemal070 <47315234+Kemal070@users.noreply.github.com>
2020-01-10 09:40:53 +0300

    navigation menu file update
    I updated the navigation menu by putting it in a new file. I'm calling a function from the pages so the navigation menu can be shown on the screen.

---
+++ lib/navigation.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:mobile_rightclick_law_native/outstanding.dart';
+
+import 'dashboard.dart';
+
+class MyNavigation extends StatefulWidget {
+  @override
+  _MyNavigationState createState() => _MyNavigationState();
+}
+
+class _MyNavigationState extends State<MyNavigation> {
+  @override
+  Widget build(BuildContext context) {
+    return  Drawer(
+      child: ListView(
+        children: <Widget>[
+          DrawerHeader(
+            child: Text(
+              'RightClick Law Menu',
+              textAlign: TextAlign.center,
+            ),
+            decoration: BoxDecoration(
+              color: Colors.green,
+            ),
+          ),
+          ListTile(
+            title: Text(
+              'Dashboard',
+              textAlign: TextAlign.center,
+            ),
+            onTap: () {
+              Navigator.pop(context, Dashboard.tag);
+            },
+          ),
+          ListTile(
+            title: Text(
+              'Outstanding',
+              textAlign: TextAlign.center,
+            ),
+            onTap: () {
+              Navigator.popAndPushNamed(context, Outstanding.tag);
+            },
+          ),
+          ListTile(
+            title: Text(
+              'Calendar',
+              textAlign: TextAlign.center,
+            ),
+            onTap: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
